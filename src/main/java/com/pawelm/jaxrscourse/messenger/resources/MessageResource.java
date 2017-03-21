849b72ffe28d8a359536bd3d151500cbc8aa99b1
Pawel M <pawmac24@gmail.com>
2017-03-21 13:27:11 +0100

    Adding @Consumes and @Produces only at level class

--- src/main/java/com/pawelm/jaxrscourse/messenger/resources/MessageResource.java
+++ src/main/java/com/pawelm/jaxrscourse/messenger/resources/MessageResource.java
@@ -18,27 +18,24 @@ import java.util.List;
  * Created by PawelM on 2017-03-20.
  */
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 
     MessageService messageService = new MessageService();
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages(){
         return messageService.getAllMessages();
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message addMessage(Message message){
         return messageService.addMessage(message);
     }
 
     @PUT
     @Path("/{messageId}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
     public Message updateMessage(@PathParam("messageId") long id, Message message){
         message.setId(id);
         return messageService.updateMessage(message);
@@ -46,14 +43,12 @@ public class MessageResource {
 
     @DELETE
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public void deleteMessage(@PathParam("messageId") long id){
         messageService.removeMessage(id);
     }
 
     @GET
     @Path("/{messageId}")
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long id){
         return messageService.getMessage(id);
     }
